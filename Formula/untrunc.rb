8dc95010de508976a84fe81e27f7d44ed528cf0a
Ben Hagen <ben@ottomatic.io>
2026-04-19 14:32:09 +0200

    Update untrunc to latest upstream revision

--- Formula/untrunc.rb
+++ Formula/untrunc.rb
@@ -1,8 +1,8 @@
 class Untrunc < Formula
   desc "Restore a truncated mp4/mov. Improved version of ponchio/untrunc"
   homepage "https://github.com/anthwlock/untrunc"
-  version "v382-2c372be"
-  url "https://github.com/anthwlock/untrunc.git", revision: "2c372be3ca591b03ca8509e74b9633b3c1c3fdcf"
+  version "v388-d286625"
+  url "https://github.com/anthwlock/untrunc.git", revision: "d286625fb51bf67b7f8536ce7d0340455d3b208b"
   license "GPL-2.0-only"
   head "https://github.com/anthwlock/untrunc.git", branch: "master"
 
